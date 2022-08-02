fbbfec296d5e35b0307d1859d4ab202bafaf0920
nands93 <femmarques93+github@gmail.com>
2022-08-02 15:36:19 -0300

    02/08/2022

---
+++ 7_Kyu/disemvowel_trolls.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+
+char *disemvowel(const char *str)
+{
+  int i = 0;
+  int j = 0;
+  char *tmp = calloc(sizeof(char), (strlen(str) + 1));
+  while(str[i] != '\0')
+  {
+    if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
+    {
+      i++;
+      continue ;
+    }
+    if (str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
+    {
+      i++;
+      continue ;
+    }
+    else
+    {
+      tmp[j] = str[i];
+      j++;
+    }
+    i++;
+  }
+  return tmp;
+}
