2018950fe79f06c00365d59889bb996db741a56b
nands93 <femmarques93+github@gmail.com>
2022-08-02 15:38:35 -0300

    02/08/2022

---
+++ 8_Kyu/reversed_strings.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include <stdlib.h>
+
+char *strrev (char *string)
+{
+  int i = 0;
+  int len = strlen(string);
+  char *tmp = calloc(sizeof(char), len + 1);
+  strcpy(tmp, string);
+  while (string[i] != '\0')
+  {
+    string[i] = tmp[len - 1];
+    i++;
+    len--;
+  }
+  string[i] = '\0';
+  return string; // reverse the string in place and return it
+}
