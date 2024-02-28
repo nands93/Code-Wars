6c6d45c8847f3984adc38d47b1bf5c90c566f36a
nands93 <femmarques93+github@gmail.com>
2024-02-28 12:42:30 -0300

    new exercise done

---
+++ 6_kyu/two_sum.c
@@ -0,0 +1,40 @@
+/*Write a function that takes an array of numbers (integers for the tests) and a target number. 
+It should find two different items in the array that, when added together, give the target value. 
+The indices of these items should then be returned in a tuple / list (depending on your language) like so: (index1, index2).
+
+For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
+
+The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers; 
+target will always be the sum of two different items from that array).
+
+Based on: https://leetcode.com/problems/two-sum/
+
+two_sum([1, 2, 3], 4) == {0, 2}
+two_sum([3, 2, 4], 6) == {1, 2}*/
+
+#include <stdlib.h>
+
+void twoSum(unsigned count, const int numbers[count], int target, unsigned *index1, unsigned *index2)
+{
+	int i = 0;
+	int j;
+	int *result = malloc(sizeof(int) * 2);
+
+	while(i < count)
+	{
+		j = i + 1;
+		while (j < count)
+		{
+			if (numbers[i] + numbers[j] == target)
+			{
+				*index1 = i;
+				*index2 = j;
+				return ;
+			}
+			j++;
+		}
+		i++;
+	}
+	free(result);
+	return ;
+}
